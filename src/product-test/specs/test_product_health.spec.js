f0987f2be9016fa88685ff64a9279c2bb702fa6f
DamianZhang <xcod19980930@gmail.com>
2022-06-02 20:31:49 +0800

    use request to catch bug

--- src/product-test/specs/test_product_health.spec.js
+++ src/product-test/specs/test_product_health.spec.js
@@ -1,24 +1,21 @@
 const { test, expect } = require('@playwright/test');
   
 test.describe('Product Health:', () => {
-  test('Liveness Should Be OK', async ({ page }) => {
-    const [response] = await Promise.all([
-      page.waitForResponse('/api/product/liveness'),
-      page.goto('/api/product/liveness'),
-    ]);
+  test('Liveness Should Be OK', async ({ request }) => {
+    const _response = await request.get('/api/product/liveness');
     console.log("------------------------------");
     console.log(await response.text());
     console.log("------------------------------");
-    expect(response.status()).toBe(200);
-    expect(response.ok()).toBeTruthy();
-    expect(await response.json()).toMatchObject({
+    expect(_response.status()).toBe(200);
+    expect(_response.ok()).toBeTruthy();
+    expect(await _response.json()).toMatchObject({
       code: 200,
       msg: 'ok'
     });
   });
 
-  test('Readiness Should Be OK', async ({ page }) => {
-    const _response = await page.goto('/api/product/readiness');
+  test('Readiness Should Be OK', async ({ request }) => {
+    const _response = await request.get('/api/product/readiness');
     expect(_response.status()).toBe(200);
     expect(_response.ok()).toBeTruthy();
     expect(await _response.json()).toMatchObject({
