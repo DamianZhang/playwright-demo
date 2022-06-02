9ccc5016c0991dcd11359d4b352934527c48b49c
DamianZhang <xcod19980930@gmail.com>
2022-06-02 19:54:24 +0800

    fix: use wait for response

--- src/product-test/specs/test_product_health.spec.js
+++ src/product-test/specs/test_product_health.spec.js
@@ -2,12 +2,15 @@ const { test, expect } = require('@playwright/test');
   
 test.describe('Product Health:', () => {
   test('Liveness Should Be OK', async ({ page }) => {
-    const _response = await page.goto('/api/product/liveness');
-    console.log(await _response.text());
+    const [response] = await Promise.all([
+      page.waitForResponse('/api/product/liveness'),
+      page.goto('/api/product/liveness'),
+    ]);
+    console.log(await response.json());
     console.log("------------------------------");
-    expect(_response.status()).toBe(200);
-    expect(_response.ok()).toBeTruthy();
-    expect(await _response.json()).toMatchObject({
+    expect(response.status()).toBe(200);
+    expect(response.ok()).toBeTruthy();
+    expect(await response.json()).toMatchObject({
       code: 200,
       msg: 'ok'
     });
