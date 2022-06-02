b1f6e40f88d396dd295c82face6a0f43016aaf09
DamianZhang <xcod19980930@gmail.com>
2022-06-02 19:30:07 +0800

    fix: added log to trace bug

--- src/product-test/specs/test_product_health.spec.js
+++ src/product-test/specs/test_product_health.spec.js
@@ -3,6 +3,8 @@ const { test, expect } = require('@playwright/test');
 test.describe('Product Health:', () => {
   test('Liveness Should Be OK', async ({ page }) => {
     const _response = await page.goto('/api/product/liveness');
+    console.log(await _response.text());
+    console.log("------------------------------");
     expect(_response.status()).toBe(200);
     expect(_response.ok()).toBeTruthy();
     expect(await _response.json()).toMatchObject({
